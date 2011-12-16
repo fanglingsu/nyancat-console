992f038d49717869ee5b26ec618cd578f53a6085
Daniel Carl <danielcarl@gmx.de>
2011-12-16 01:56:00 +0100

    Simplified and optimized nyans walking cycle.
    Now nyan looks really squirrelly.

--- src/cat.c
+++ src/cat.c
@@ -65,13 +65,9 @@ void print_cat(void)
         char str[8];
     } feets[] = {
         {x + 1, "U U U U"},
-        {x + 1, "U U UU"},
         {x + 1, "UU  UU"},
-        {x + 1, "UU  U"},
         {x + 2, "U   U"},
-        {x + 2, "U   UU"},
         {x + 2, "UU  UU"},
-        {x + 2, "UU U U"}
     };
 
     wattron(nc.ui.world, COLOR_PAIR(ColorMagenta));
